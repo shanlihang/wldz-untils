ff901a58e6109421d8c2a56c0e29c6ed85b618ed
shanlihang <slh_ontuo@126.com>
2024-02-01 13:27:03 +0800

    日期格式和基础格式化的封装

--- 时间与日期处理/date.md
+++ 时间与日期处理/date.md
@@ -1,1 +1,33 @@
-### 时间与日期处理
+```
+时间与日期处理工具类
+本工具库基于day.js 1.11.10
+```
+
+---
+
+## 时间日期格式
+
+##### 默认格式
+1. 日期格式：YYYY-MM-DD
+2. 时间格式：hh:mm:ss
+3. 日期时间格式：YYYY-MM-DD hh:mm:ss
+
+##### 修改默认格式
+1. 修改日期格式：setDateSign(sign)
+2. 修改时间格式：setTimeSign(sign)
+3. 修改日期时间格式：setDateTimeSign(sign)
+> 参数传入想要的格式字符串
+
+--- 
+
+## 时间日期操作
+
+##### 格式化操作
+1. 格式化日期：dateFormat(date)
+2. 格式化时间：timeFormat(date)
+3. 格式化日期时间：dateTimeFormat(date)
+> 参数可以为日期，字符串，时间戳格式
+> 不传入参数默认为对当前时间进行格式化
+
+---
+
---
+++ 时间与日期处理/date.ts
@@ -0,0 +1,34 @@
+import * as dayjs from 'dayjs'
+
+// 默认格式化字符串
+export let date_sign:string = 'YYYY-MM-DD'
+export let time_sign:string = 'hh:mm:ss'
+export let date_time_sign:string = 'YYYY-MM-DD hh:mm:ss'
+
+// 设置日期格式
+export const setDateSign = (sign:string) => {
+    date_sign = sign
+}
+// 设置时间格式
+export const setTimeSign = (sign:string) => {
+    time_sign = sign
+}
+// 设置时间日期格式
+export const setDateTimeSign = (sign:string) => {
+    date_time_sign = sign
+}
+
+// 格式化日期
+export const dateFormat = (date:number | string | Date = new Date()) => {
+    return dayjs(date).format(date_sign);
+}
+
+// 格式化时间
+export const timeFormat = (date:number | string | Date = new Date()) => {
+    return dayjs(date).format(time_sign);
+}
+
+// 格式化日期时间
+export const dateTimeFormat = (date:number | string | Date = new Date()) => {
+    return dayjs(date).format(date_time_sign);
+}
